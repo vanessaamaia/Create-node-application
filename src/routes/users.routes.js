dcc8fd7e5c38c0e6b7c3cf3002718a4649949682
VanessaMaia26 <vanessamaia2607@gmail.com>
2023-10-24 20:41:38 -0300

    Middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express');
 
-const usersRoutes = Router();
-
 const UsersController = require("../controllers/UsersController");
+
+const usersRoutes = Router();
 
 const usersController = new UsersController();
 
-usersRoutes.post('/', usersController.create);
+usersRoutes.post('/', myMiddleware, usersController.create);
 
 module.exports = usersRoutes;
