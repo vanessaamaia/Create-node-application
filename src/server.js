d87f21a877767b4b9b3f16da3da01cd8e7b44d17
VanessaMaia26 <vanessamaia2607@gmail.com>
2023-10-24 15:04:07 -0300

    POST method

--- src/server.js
+++ src/server.js
@@ -2,22 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.get("/message/:id/:user", (request, response) => {
-    const { id, user } = request.params;
-
-    response.send(`
-      Mensagem ID: ${id}.
-      Para o usuário: ${user}.
-    `);
-});
-
-app.get("/users", (request, response) => {
+app.post("/users", (request, response) => {
     const { page, limit } = request.query;
 
-    response.send(`
-      Página: ${page}.
-      Mostrar: ${limit}.
-    `);
+    response.send("Você chamou o POST");
 });
 
 const PORT = 3333;
