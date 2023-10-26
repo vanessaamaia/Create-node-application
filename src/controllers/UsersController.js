6f4c7c5e570caedf1e97e4d11f10c9cacaeb728b
VanessaMaia26 <vanessamaia2607@gmail.com>
2023-10-26 19:40:58 -0300

    Updating user

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,27 +1,59 @@
-const { hash } = require("bcryptjs");
-const AppError = require('../utils/AppError');
-const sqliteConnection = require("../database/sqlite");
+const { hash } = require('bcryptjs')
+const AppError = require('../utils/AppError')
+const sqliteConnection = require('../database/sqlite')
 
 class UsersController {
   async create(request, response) {
-    const { name, email, password } = request.body;
+    const { name, email, password } = request.body
 
-    const database = await sqliteConnection();
-    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-    if (checkUserExists) {
-      throw new AppError ("Este e-mail já está em uso.");
+    if (checkUserExists){
+      throw new AppError("Este e-mail já está em uso.")
     }
 
-    const hashedPassword = await hash(password, 8);
+    const hashedPassword = await hash(password, 8)
 
-    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-    [name, email, hashedPassword]
-    );
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, hashedPassword]
+    )
 
-    return response.status(201).json();
-
+    return response.status(201).json()
   }
-};
 
-module.exports = UsersController;
+  async update(request, response) {
+     const { name, email } = request.body
+     const { id } = request.params
+
+     const database = await sqliteConnection()
+     const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+     if(!user) {
+      throw new AppError("Usuário não encontrado")
+     }
+
+     const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+     if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.")
+     }
+
+     user.name = name
+     user.email= email
+
+     await database.run(`
+      UPDATE users SET
+      name = ?,
+      email = ?,
+      updated_at = ?
+      WHERE id = ?`, 
+      [user.name, user.email, new Date(), id]
+    )
+
+    return response.json()
+  }
+}
+
+module.exports = UsersController
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,12 @@
-const { Router } = require('express')
+const { Router } = require('express');
 
-const UsersController = require('../controllers/UsersController')
+const UsersController = require('../controllers/UsersController');
 
-const usersRoutes = Router()
+const usersRoutes = Router();
 
-const usersController = new UsersController()
+const usersController = new UsersController();
 
-usersRoutes.post('/', usersController.create)
+usersRoutes.post('/', usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
-module.exports = usersRoutes
+module.exports = usersRoutes;
