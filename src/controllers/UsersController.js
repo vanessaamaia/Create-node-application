e006edb053ee432c8783c27babbf3ebc6129be4a
VanessaMaia26 <vanessamaia2607@gmail.com>
2023-10-31 20:59:42 -0300

    reviewing errors

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -63,7 +63,7 @@ class UsersController {
       email = ?,
       password = ?,
       updated_at = DATETIME('now')
-      WHERE id = ?`,
+      WHERE id = ?`, 
       [user.name, user.email, user.password, id]
     )
 
